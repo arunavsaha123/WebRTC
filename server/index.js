278d1218d89fda88330144d4d6479764a0cca29a
arunavsaha123 <arunavsaha984@gmail.com>
2024-08-11 15:49:52 +0530

    server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
-const { Server } = require("socket.io");
-const io = new Server(8000, {
-    cors: true,
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+// Initialize Express
+const app = express();
+
+// Create an HTTP server
+const server = http.createServer(app);
+
+// Initialize Socket.io with the HTTP server
+const io = new Server(server, {
+    cors: {
+        origin: 'https://web-rtc-pearl.vercel.app', // You can configure the allowed origin here
+    },
 });
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
 const emailToSocketMap = new Map();
 const socketidToEmailMap = new Map();
 
@@ -14,4 +31,9 @@ io.on("connection", (socket) => {
         io.to(room).emit("user:joined", { email, id: socket.id })
         io.to(socket.id).emit("join:room", data)
     });
+});
+
+const PORT = process.env.PORT || 8000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
